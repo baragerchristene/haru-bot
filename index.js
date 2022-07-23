e4c9d1036b68a714a9a7683042b8bde922cbcd52
Hưởng <huongnv.ous@mcredit.com.vn>
2022-07-23 15:41:55 +0700

    fix req not completed

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 const lib = require("./lib");
 
-/* GET home page. */
+/* GET ping page. */
 router.get('/ping', function(_req, res, _next) {
   lib.sendServerStatus().then(_r => {});
   res.json({message: 'pong'});
+  res.end();
 });
 
 module.exports = router;
