0a8341048b5527af8a1a15aeaa034c2ce42d5797
Hưởng <huongnv.ous@mcredit.com.vn>
2022-08-20 12:12:31 +0700

    update dca message

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ async function main() {
                                     if (!_.isEmpty(myPosition)) { // có vị thế rồi thì DCA thêm
                                         let amountChange = Math.abs(myPosition.positionAmt * amountChangeRate).toFixed(3);
                                         if (amountChange == 0) amountChange = lib.getMinQty(myPosition, filterSymbols); // amount bằng 0 thì lấy min
-                                        await lib.dcaPositionByType(newSide, leadPosition.symbol, amountChange);
+                                        await lib.dcaPositionByType(newSide, leadPosition.symbol, amountChange, oldAmt, newAmt, leadPositionOld.entryPrice, leadPosition.entryPrice);
                                     } else { // chưa có thì tạo mới
                                         let minAmount = lib.getMinQty(leadPosition, filterSymbols);
                                         await lib.openPositionByType(newSide, leadPosition.symbol, minAmount, leadPosition.leverage)
