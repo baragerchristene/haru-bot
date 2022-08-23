36521adda871d00b630c4587e446398278be7645
Hưởng <huongnv.ous@mcredit.com.vn>
2022-08-23 09:17:07 +0700

    update telegram command

--- telegram.js
+++ telegram.js
@@ -1,11 +1,6 @@
 const {Telegraf} = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const group_id = process.env.GROUP_ID;
-
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch().then(r => {})
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
