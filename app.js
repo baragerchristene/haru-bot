7fecff8fa5f6e7e20f22e13906dd5e156cae5ed5
Hưởng <huongnv.ous@mcredit.com.vn>
2022-10-18 21:27:24 +0700

    remove btc from the trade list

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ async function CopyStream() {
     bot.InitialData();
     // bot.BinanceCopier().then();
     // // can do it with a loop but todo later
-    bot.strategyOCC('BTCBUSD').then();   bot.AutoTakingProfit('BTCBUSD').then();
     bot.strategyOCC('ETHUSDT').then();   bot.AutoTakingProfit('ETHUSDT').then();
     bot.strategyOCC('XRPUSDT').then();   bot.AutoTakingProfit('XRPUSDT').then();
     bot.strategyOCC('BNBUSDT').then();   bot.AutoTakingProfit('BNBUSDT').then();
