c88f3c3cb70cf395fafe3c368152aa16fcdc2d9a
Hưởng <huongnv.ous@mcredit.com.vn>
2022-11-04 11:34:14 +0700

    update interval to 1ms

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ const server            = http.createServer(app);
 const wss               = new WebSocketServer({ port: 13456 });
 const bot               = require('./bot');
 app.use('/', indexRouter); app.set('port', port);
-wss.on('connection', ws => {setInterval(() => { ws.send('ok') }, 500)});
+wss.on('connection', ws => {setInterval(() => { ws.send('ok') }, 1000)});
 server.listen(port);
 /**
  * BOT COPY
  */
 async function CopyStream() {
     await bot.InitialData();
-//     bot.autoSyncExchange().then();
-//     bot.autoSyncIgnorer().then();
-//     bot.binanceCopier().then();
-//     bot.invertBinanceCopier().then();
+    bot.autoSyncExchange().then();
+    bot.autoSyncIgnorer().then();
+    bot.binanceCopier().then();
+    bot.invertBinanceCopier().then();
 }
 
 CopyStream().then() // profit go here
