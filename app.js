4480f772b460c1c000737718f0f6cd3f981b73f7
Hưởng <huongnv.ous@mcredit.com.vn>
2022-11-06 17:27:52 +0700

    temporary comment code

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-// const {WebSocketServer} = require("ws");
-// const indexRouter       = require('./index');
-// const express           = require("express");
-// const http              = require("http");
-// const app               = express();
-// const port              = process.env.PORT || 3000;
-// const server            = http.createServer(app);
-// const wss               = new WebSocketServer({ port: 13456 });
 const BotUI             = require('./botUI');
-// const bot               = require('./bot');
-// app.use('/', indexRouter); app.set('port', port);
-// wss.on('connection', ws => {setInterval(() => { ws.send('ok') }, 1000)});
-// server.listen(port);
 /**
  * BOT COPY
  */
 async function CopyStream() {
     const bot = new BotUI();
-    await bot.getLastSession();
+    // await bot.getLastSession();
     bot.autoSyncExchanges();
     bot.autoSyncIgnores();
     bot.autoBinanceCopier();
