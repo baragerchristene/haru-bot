4f52f145fb92e71d636e34e22f7e07d5c8662533
Hưởng <huongnv.ous@mcredit.com.vn>
2022-11-14 10:02:19 +0700

    update ws import

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const {WebSocketServer} = require("ws");
 const wss               = new WebSocketServer({ port: 13456 });
 const wss1ms            = new WebSocketServer({ port: 13457 });
 
-wss.on('connection', ws => {setInterval(() => { ws.send('ok') }, 500)});
-wss1ms.on('connection', ws => {setInterval(() => { ws.send('ok') }, 1000)});
+wss.on('connection', ws => {setInterval(() => { ws.send('ok') }, 1000)});
+wss1ms.on('connection', ws => {setInterval(() => { ws.send('ok') }, 5000)});
 
 /**
  * BOT COPY
