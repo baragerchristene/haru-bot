ba0708dd3ec3af20421a9582a6e7392433784fb4
Hưởng <huongnv.ous@mcredit.com.vn>
2022-11-14 21:44:59 +0700

    Revert "update filterSymbols"
    This reverts commit e427b67ce6c5bbdeacbe66928100f8de2267ce21.

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const BotUI             = require('./botUI');
 
 const {WebSocketServer} = require("ws");
 const wss               = new WebSocketServer({ port: 13456 });
+const wss1ms            = new WebSocketServer({ port: 13457 });
+
 wss.on('connection', ws => {setInterval(() => { ws.send('ok') }, 500)});
+wss1ms.on('connection', ws => {setInterval(() => { ws.send('ok') }, 5000)});
 
 /**
  * BOT COPY
  */
 async function CopyStream() {
     const bot = new BotUI();
+    await bot.getLastSession();
+    bot.autoSyncExchanges().then();
     bot.autoBinanceCopier().then();
 }
 
