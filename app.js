5bc6990bd149da67c4f76eda87fe561412f667e9
Hưởng <huongnv.ous@mcredit.com.vn>
2022-11-14 22:22:43 +0700

    update save config before crashed

--- app.js
+++ app.js
@@ -3,14 +3,6 @@ const BotUI             = require('./botUI');
 const {WebSocketServer} = require("ws");
 const wss               = new WebSocketServer({ port: 13456 });
 const wss1ms            = new WebSocketServer({ port: 13457 });
-var http                = require('http');
-const port = process.env.PORT || 3000;
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    //Return the url part of the request object:
-    res.write(req.url);
-    res.end();
-}).listen(port);
 wss.on('connection', ws => {setInterval(() => { ws.send('ok') }, 500)});
 wss1ms.on('connection', ws => {setInterval(() => { ws.send('ok') }, 2000)});
 
